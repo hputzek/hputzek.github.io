a7ae85e43ebb4eb2bfdb89d777819318e83a954b
Hendrik Putzek <hendrik.putzek@dkd.de>
2016-01-12 16:28:46 +0100

    Changed template completely

---
+++ Gruntfile.js
@@ -0,0 +1,84 @@
+module.exports = function(grunt) {
+
+    // Load NPM Tasks
+    // https://github.com/shootaroo/jit-grunt
+    require('jit-grunt')(grunt, {
+
+    });
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON( 'package.json' ),
+
+        config: {
+            src: './src',
+            dist: '.'
+        },
+
+        watch: {
+            files: '<%= config.src %>/scss/**/*.scss',
+            tasks: ['sass','autoprefixer','cssUrlEmbed']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        '.tmp/*.css',
+                        '<%= config.src %>/*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: ['<%= config.src %>/','./']
+                }
+            }
+        },
+
+        inline: {
+            dist: {
+                options:{
+                    uglify: true,
+                    cssmin: true
+                },
+                src: '<%= config.src %>/index.html',
+                dest: '<%= config.dist %>/index.html'
+
+            }
+        },
+
+        sass: {
+            options: {
+                sourceMap: false
+            },
+            dist: {
+                files: {
+                    '.tmp/main.css': '<%= config.src %>/scss/screen.scss'
+                }
+            }
+        },
+
+        cssUrlEmbed: {
+            encodeDirectly: {
+                options: {
+                    baseDir: 'src/scss'
+                },
+                files: {
+                    '.tmp/main.css': ['.tmp/main.css']
+                }
+            }
+        },
+
+        autoprefixer: {
+            options: {
+                // Task-specific options go here.
+            },
+            dist: {
+                // Target-specific file lists and/or options go here.
+                src: '.tmp/main.css'
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build', ['sass','cssUrlEmbed','autoprefixer','inline']);
+}
